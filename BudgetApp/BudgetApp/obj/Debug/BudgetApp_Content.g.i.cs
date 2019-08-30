525cb3718a002e33353c6ce908b4d287063a850a
TheOverwatcher <TheOverwatcher@users.noreply.github.com>
2019-08-30 13:51:00 -0400

    DataSet creatopm
    Created data set to accompany database init

--- BudgetApp/BudgetApp/obj/Debug/BudgetApp_Content.g.i.cs
+++ BudgetApp/BudgetApp/obj/Debug/BudgetApp_Content.g.i.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sampledatabase.mdf")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sampledatabase_log.ldf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("budgetappdatabase.mdf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("budgetappdatabase_log.ldf")]
 
 
