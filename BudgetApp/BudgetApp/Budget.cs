e66da0c6dd2735d4c51a026104cde3ba730bcaeb
TheOverwatcher <TheOverwatcher@users.noreply.github.com>
2019-09-22 22:03:20 -0400

    Changes were untracked

---
+++ BudgetApp/BudgetApp/Budget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    public class Budget
+    {
+        public Budget(int id, string name)
+        {
+            this.BudgetId = id;
+            this.BudgetName = name;
+        }
+
+        public string BudgetName { get; set; }
+
+        public int BudgetId { get; set; }
+
+    }
+}
---
+++ BudgetApp/BudgetApp/BudgetFormViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    class BudgetFormViewModel : BaseViewModel
+    {
+        public BudgetFormViewModel()
+        {
+
+        }
+
+        public BudgetFormViewModel(string name)
+        {
+            this.BudgetName = name;
+        }
+
+        private string _budgetName = "Example Name";
+        public string BudgetName
+        {
+            get { return _budgetName; }
+            set { SetProperty(ref _budgetName, value); }
+        }
+    }
+}
