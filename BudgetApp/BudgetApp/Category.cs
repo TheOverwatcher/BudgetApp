5b404b26698a735a5251edc648dd3eff5830e6cd
TheOverwatcher <TheOverwatcher@users.noreply.github.com>
2019-09-27 22:50:31 -0400

    Add category files

---
+++ BudgetApp/BudgetApp/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    class Category
+    {
+        public Category(int id, string name)
+        {
+            this.CategoryId = id;
+            this.CategoryName = name;
+        }
+
+        public string CategoryName { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ BudgetApp/BudgetApp/CategoryManagementViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    class CategoryManagementViewModel : BaseViewModel
+    {
+        public CategoryManagementViewModel()
+        {
+
+        }
+
+        public CategoryManagementViewModel(string name, int id)
+        {
+            this.CategoryName = name;
+            this.CategoryId = id;
+        }
+
+        private string _categoryName = "Example Category Name";
+        public string CategoryName
+        {
+            get { return _categoryName; }
+            set { SetProperty(ref _categoryName, value); }
+        }
+
+        private int _categoryId = -1;
+        public int CategoryId
+        {
+            get { return _categoryId; }
+            set { SetProperty(ref _categoryId, value); }
+        }
+    }
+}
